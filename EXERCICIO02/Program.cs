3c14132d0f3a0aa0e6283043dc59fcac312c945c
kemily <kemilyteixeira047@gmail.com>
2023-02-06 22:06:55 -0300

    start repo

---
+++ EXERCICIO02/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EXERCICIO02 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
